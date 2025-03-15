629d25d002a1df9caf2724d8e61d63d9c18c7220
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-03-15 15:51:17 +0900

    테스트 데이터 추가

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-COPY src/requirements.txt /app
+COPY test/requirements.txt /app
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY test /app
--- test/test.py
+++ test/test.py
@@ -4,23 +4,28 @@ model_size = "large-v3"
 
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-filename = []
+filenames = [
+    "1.wav",
+    "2.wav",
+    "3.wav",
+    "4.wav",
+]
 
-segments, info = model.transcribe(
-    # f"/Users/mousebook/Documents/side project/2024hack/test/STT/music/4_6f1ce97a-cb5d-49a7-8ce9-38672133be17.wav",
-    f"/app/music/4_6f1ce97a-cb5d-49a7-8ce9-38672133be17.wav",
-    beam_size=5,
-)
+for filename in filenames:
+    segments, info = model.transcribe(
+        f"/app/music/{filename}",
+        beam_size=5,
+    )
 
-print(
-    "Detected language '%s' with probability %f"
-    % (info.language, info.language_probability)
-)
+    print(
+        "Detected language '%s' with probability %f"
+        % (info.language, info.language_probability)
+    )
 
-stt_result = ""
+    stt_result = ""
 
-for segment in segments:
-    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-    stt_result += segment.text
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+        stt_result += segment.text
 
-print(stt_result)
+    print(stt_result)
